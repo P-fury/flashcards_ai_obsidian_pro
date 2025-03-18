3a9657cbcb1446f17abffa5ed4d3421400b152b0
Piotr Plucinski <plutocamera@gmail.com>
2025-03-18 22:24:34 +0100

    test .env config

--- app/main.py
+++ app/main.py
@@ -1,24 +1,44 @@
 import atexit
 import json
 import logging.config
+import os
+import tomllib
+from logging import Handler
 from pathlib import Path
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
 
-def setup_logging():
-    config_file = Path(".logging_configs/base_config.json")
-    with open(config_file, 'r') as file:
-        config = json.load(file)
+# JSON CONFIG
+# def setup_logging():
+#     config_file = Path(".logging_configs/base_config.json")
+#     with open(config_file, 'r') as file:
+#         config = json.load(file)
+#     logging.config.dictConfig(config)
+#
+#     queue_handler = logging.getHandlerByName("queue_handler")
+#     if queue_handler is not None:
+#         queue_handler.listener.start()
+#         atexit.register(queue_handler.listener.stop)
+
+
+def setup_logging() -> None:
+    if os.getenv("TEST") == "1":
+        return None
+    config_file: Path = Path(".logging_configs/config.toml")
+    with open(config_file, 'rb') as file:
+        config: dict[str, Any] = tomllib.load(file)
+
     logging.config.dictConfig(config)
 
-    queue_handler = logging.getHandlerByName("queue_handler")
+    queue_handler: Handler | None = logging.getHandlerByName("queue_handler")
     if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
+        queue_handler.listener.start()  # type: ignore [attr-defined]
+        atexit.register(queue_handler.listener.stop)  # type: ignore [attr-defined]
 
 
-def main():
+def main() -> None:
     setup_logging()
     logger.debug("debug message")
     logger.info("info message")
